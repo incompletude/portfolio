c14c9c1b9ff49f330af78f6e6f9778ae4869dc6b
André Freitas <contato@andrefreitas.dev>
2019-12-13 16:28:24 -0300

    alfa nuxt ssr serverless

---
+++ src/portfolio/client/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>v1</h1>
+    <h3>Random dog of the day:</h3>
+    <img :src="dog.url" alt />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, $http }) {
+    const dogs = await $http.$get(
+      "https://api.thedogapi.com/v1/images/search?limit=1"
+    );
+    return { dog: dogs[0] };
+  }
+};
+</script>
