81ff184e0b1e23fad7e75b2076343b285901d08a
André Freitas <contato@andrefreitas.dev>
2019-12-26 19:35:51 -0300

    cleaning projects

--- contents/projects.js
+++ contents/projects.js
@@ -1,98 +1,95 @@
 class ProjectFactory {
   constructor() {
     return new Promise(async (resolve, reject) => {
-      this.projects = await this.asyncGetAll();
-      resolve(this);
-    });
+      this.projects = await this.asyncGetAll()
+      resolve(this)
+    })
   }
 
   getAllSlugs() {
     return [
-      'remessa-online',
-      'jeunesse',
-      'nestle-nesfit',
-      'microsoft',
-      'chevrolet',
-      'mastercard',
-      'wmccann',
-      'tim',
-      'deca',
-      'nestle-institutional',
-      'barretos-cancer-hospital',
-      'the-salvation-army'
+      "remessa-online",
+      "jeunesse",
+      "nestle-nesfit",
+      "microsoft",
+      "chevrolet",
+      "mastercard",
+      "wmccann",
+      "tim",
+      "deca",
+      "nestle-institutional",
+      "barretos-cancer-hospital",
+      "the-salvation-army"
     ]
   }
 
   async asyncImport(slug) {
-    const md = await import(`~/contents/projects/${slug}.md`);
-    return md;
+    const md = await import(`~/contents/projects/${slug}.md`)
+    return md
   }
 
   async asyncGetAll() {
     const projects = await Promise.all(
       this.getAllSlugs().map(slug => this.asyncImport(slug))
     ).then(response => {
-      return response;
-    });
+      return response
+    })
 
-    return projects;
+    return projects
   }
 
   findByFeatured() {
-    return this.projects.find(p => p.attributes.featured === true);
+    return this.projects.find(p => p.attributes.featured === true)
   }
 
   getByPartners() {
-    return this.projects.slice(0, 12);
+    return this.projects.slice(0, 12)
   }
 
   getByRecent() {
-    return this.projects.slice(0, 4);
+    return this.projects.slice(0, 4)
   }
 
   getDistinctCategories() {
     return Object.entries(Object.assign({}, ...this.projects.map(p => p.attributes.categories)))
-      .map(([slug, name]) => ({ slug, name })).sort((a, b) => (a.slug > b.slug) ? 1 : -1);
+      .map(([slug, name]) => ({ slug, name }))
+      .sort((a, b) => (a.slug > b.slug) ? 1 : -1)
   }
 
   getDistinctTags() {
     return Object.entries(Object.assign({}, ...this.projects.map(p => p.attributes.tags)))
-      .map(([slug, name]) => ({ slug, name })).sort((a, b) => (a.slug > b.slug) ? 1 : -1);
+      .map(([slug, name]) => ({ slug, name }))
+      .sort((a, b) => (a.slug > b.slug) ? 1 : -1)
   }
 
   getDistinctYears() {
-    return this.projects.map(p => p.attributes.year).filter((e, i, arr) => {
-      return arr.indexOf(e) === i;
-    });
+    return this.projects.map(p => p.attributes.year).filter((e, i, arr) => arr.indexOf(e) === i)
   }
 
   getPage(featured, category, tag, year, page) {
-    if (isNaN(page) || isNaN(year)) return false;
+    if (isNaN(page) || isNaN(year)) return false
 
-    let projects = null;
+    let projects = null
 
-    if (featured) projects = this.projects.filter(p => p.attributes.featured === true);
-    else if (category) projects = this.projects.filter(p => {
-      console.log(p.attributes.categories)
-      return p.attributes.categories.hasOwnProperty(category)
-    });
-    else if (tag) projects = this.projects.filter(p => p.attributes.tags.hasOwnProperty(tag));
-    else if (year) projects = this.projects.filter(p => p.attributes.year === year);
-    else projects = this.projects;
+    if (featured) projects = this.projects.filter(p => p.attributes.featured === true)
+    else if (category) projects = this.projects.filter(p => p.attributes.categories.hasOwnProperty(category))
+    else if (tag) projects = this.projects.filter(p => p.attributes.tags.hasOwnProperty(tag))
+    else if (year) projects = this.projects.filter(p => p.attributes.year === year)
+    else projects = this.projects
 
-    if (projects.length === 0) return false;
+    if (projects.length === 0) return false
 
-    const projectsCount = this.projects.length;
-    const pageCount = Math.ceil(projects.length / 10);
+    const projectsCount = this.projects.length
+    const pageCount = Math.ceil(projects.length / 10)
 
-    projects = projects.slice((page - 1) * 10, page * 10);
+    projects = projects.slice((page - 1) * 10, page * 10)
 
     return { projects: projects, currentPage: page, pageCount: pageCount, projectsCount: projectsCount }
   }
 
   find(slug) {
-    return this.projects.find(p => p.attributes.slug === slug);
+    return this.projects.find(p => p.attributes.slug === slug)
   }
 }
 
-export default ProjectFactory;
+export default ProjectFactory
